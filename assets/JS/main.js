f795da82958383577a58a413119138102e15dbf9
harkanni <harkanzyempire001@gmail.com>
2022-10-15 00:18:24 +0100

    added mobile responsive css

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,4 +1,4 @@
-// FORM MODAL TOGGLING
+// SEARCH BAR TOGGLING
 const searchIcon = document.querySelector(".utilities .fa")
 const searchModal = document.querySelector(".form-search")
 const closeModal = document.querySelector(".fa-window-close")
@@ -9,3 +9,28 @@ closeModal.addEventListener("click", () => {
 	searchModal.classList.remove('active')
 })
 searchModal.hasOwnProperty("active")
+
+// SIDEBAR TOGGLE
+const menuOpen = document.querySelector(".menu-bar")
+const menuClose = document.querySelector(".sideBar-container .fa-window-close")
+
+menuOpen.addEventListener("click", () => {
+  var sideBar = document.querySelector(".sideBar-container")
+  sideBar.classList.add("active")
+})
+menuClose.addEventListener("click", () => {
+  var sideBar = document.querySelector(".sideBar-container")
+  sideBar.classList.remove("active")
+})
+
+// SELECTION CATALOGUE TAB
+const tabs = document.querySelectorAll(".nav-tab div")
+tabs.forEach((tab) => {
+	tab.addEventListener("click", () => {
+		tabs.forEach((tab)=>{
+			tab.classList.remove("active")
+		})
+		tab.classList.add("active")
+	})
+})
+console.log(tabs)
