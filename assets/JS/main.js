a85fe5f3a722b881a67ba7b3283f5de758f796e5
Akanni Emmanuel <67793955+Harkanni@users.noreply.github.com>
2023-04-24 23:43:00 +0100

    Update main.js

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,5 +1,5 @@
 // SEARCH BAR TOGGLING
-const searchIcon = document.querySelector(".utilities .fa6")
+const searchIcon = document.querySelector(".utilities .fa7")
 const searchModal = document.querySelector(".form-search")
 const closeModal = document.querySelector(".fa-window-close")
 searchIcon.addEventListener("click", () => {
